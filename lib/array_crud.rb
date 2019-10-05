16e192f0b7b5558648aad6696c003ee26c5e1b72
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-05 18:16:23 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,46 @@
 def create_an_empty_array
-  
+    return Array.new
 end
 
 def create_an_array
-  
+    new_array = ['mike','john','philip','monkey']
+    return new_array
+   
 end
 
 def add_element_to_end_of_array(array, element)
+   return array << element
   
 end
 
 def add_element_to_start_of_array(array, element)
+   return array.unshift('wow')
   
 end
 
 def remove_element_from_end_of_array(array)
-  
+    return array.pop
 end
 
 def remove_element_from_start_of_array(array)
+    return array.shift
   
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+   return array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
+    return array[0]
   
 end
 
 def retrieve_last_element_from_array(array)
+   return array[-1]
   
 end
 
 def update_element_from_index(array, index_number, element)
-
+    return array[index_number] = 'totally'
 end
