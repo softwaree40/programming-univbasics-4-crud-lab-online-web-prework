8ef5c4a5e8148ba78a8e6758778f5104f6dc2c11
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-10 23:30:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,11 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
+<<<<<<< HEAD
    return array[index_number]
+=======
+   return array.index_number('am')
+>>>>>>> db8dd85b3128d6a050737659012562c9b1e5e02e
 end
 
 def retrieve_first_element_from_array(array)
@@ -42,5 +46,11 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
+<<<<<<< HEAD
     return array[index_number] = 'totally'
 end
+=======
+    return array[index_number] = 'element'
+
+end
+>>>>>>> db8dd85b3128d6a050737659012562c9b1e5e02e
